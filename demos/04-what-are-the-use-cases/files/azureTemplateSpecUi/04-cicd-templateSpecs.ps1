16a3429da2efb7259efd8be8cb243f41758fa2f1
Georgi Ivanov <georgimilchevivanov@kpmg.com>
2024-02-26 23:21:04 +0200

    more samples

--- demos/04-what-are-the-use-cases/files/azureTemplateSpecUi/04-cicd-templateSpecs.ps1
+++ demos/04-what-are-the-use-cases/files/azureTemplateSpecUi/04-cicd-templateSpecs.ps1
@@ -9,7 +9,21 @@ Get-PSPlaceholder -Path $templatesFolderPath | Format-Table name, type
 
 #update placeholders
 Update-PSPlaceholder -Path $templatesFolderPath -Values @{
-    uiStrings = $phValues
-    environmentType = 'dev'
-    allowedDeploymentSubscriptions= @('sub1', 'sub2')
+    uiStrings                      = $phValues
+    environmentType                = 'dev'
+    allowedDeploymentSubscriptions = @('MSDN Platforms Subscription', 'Visual Studio Professional Subscription')
 } -AdaptTo json
+
+#publish templateSpec
+$newAzTemplateSpecParams = @{
+    ResourceGroupName    = 'psdaych2024'
+    Name                 = 'sqlServer'
+    Version              = '1.0.0'
+    Description          = 'Test sql server'
+    DisplayName          = 'Sql Server'
+    TemplateFile         = 'C:\events\psdaych2024\demos\04-what-are-the-use-cases\files\azureTemplateSpecUi\templates\sqlServer\sqlServer.json'
+    VersionDescription   = 'Initial version'
+    Location             = 'westeurope'
+    UIFormDefinitionFile = 'C:\events\psdaych2024\demos\04-what-are-the-use-cases\files\azureTemplateSpecUi\templates\sqlServer\sqlServer.ui.jsonc'
+}
+New-AzTemplateSpec @newAzTemplateSpecParams -ErrorAction Stop
