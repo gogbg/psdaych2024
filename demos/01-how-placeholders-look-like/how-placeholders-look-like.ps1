0eb9eda5c287af080e24f09d517cd67f5d1a8839
Georgi Ivanov <georgimilchevivanov@kpmg.com>
2024-02-26 19:44:21 +0200

    add demo files

---
+++ demos/01-how-placeholders-look-like/how-placeholders-look-like.ps1
@@ -0,0 +1,26 @@
+#should return: 'My name is John'
+Update-PSPlaceholder -String 'My name is {{FirstName}}' -Values @{FirstName='John'}
+
+#should return: 'My name is John Dow'
+Update-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' -Values @{
+  FirstName ='John'
+  LastName  ='Dow'
+}
+
+#should update all placeholders in the file sampleFile.txt
+$filePath = 'C:\events\psdaysch\demos\01-how-placeholders-look-like\files\sampleFile.txt'
+Update-PSPlaceholder -Path $filePath -Values @{
+  FirstName ='John'
+  LastName  ='Dow'
+  yearInSchool = 'second'
+  today = get-date
+}
+
+#should update all placeholders in all files underneath the specified folder
+$folderPath = 'C:\events\psdaysch\demos\01-how-placeholders-look-like\files\someFolder'
+Update-PSPlaceholder -Path $folderPath -Values @{
+  FirstName ='John'
+  LastName  ='Dow'
+  yearInSchool = 'second'
+  today = get-date
+}
