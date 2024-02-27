39ed00d1d9a082f2ca0a130c93ba57fb193bfa6f
Georgi Ivanov <georgimilchevivanov@kpmg.com>
2024-02-27 09:20:33 +0200

    add more samples

--- demos/04-what-are-the-use-cases/parameterFiles/04-parameterFiles.ps1
+++ demos/04-what-are-the-use-cases/parameterFiles/04-parameterFiles.ps1
@@ -4,9 +4,13 @@ $paramFolderPath = 'C:\events\psdaych2024\demos\04-what-are-the-use-cases\parame
 Get-PSPlaceholder -Path $paramFolderPath | ft name, type, InFileCount, InFileNameCount
 
 #create temp folder for the updated parameter files
-$tempFolder = Split-Path $paramFolderPath | Join-Path -ChildPath 'temp'
+$tempFolder = Split-Path $paramFolderPath | Join-Path -ChildPath 'outputParamFiles'
 if (Test-Path -Path $tempFolder) {
     Remove-Item -Path $tempFolder -Recurse -Force
 }
-New-Item -Path $tempFolder -ItemType Directory
+$null = New-Item -Path $tempFolder -ItemType Directory
 Copy-Item -Path $paramFolderPath -Destination $tempFolder -Recurse -Force
+
+Update-PSPlaceholder -Path $tempFolder -Values @{
+    #to fill param values
+} -AdaptTo json
