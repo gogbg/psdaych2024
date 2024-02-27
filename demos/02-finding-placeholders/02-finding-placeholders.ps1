4fddb388c8e6e1dfb27543bcdb9123d781a08db0
Georgi Ivanov <georgimilchevivanov@kpmg.com>
2024-02-27 11:00:46 +0200

    update samples

--- demos/02-finding-placeholders/02-finding-placeholders.ps1
+++ demos/02-finding-placeholders/02-finding-placeholders.ps1
@@ -1,13 +1,17 @@
 #should return used placeholders in string
+cls
 Get-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' | ft
 
 #should return used placeholders in specific file
+cls
 Get-PSPlaceholder -Path 'C:\events\psdaych2024\demos\02-finding-placeholders\files\sampleFile.txt' | ft
 
 #should return used placeholders in all files underneath the specified folder
+cls
 Get-PSPlaceholder -Path 'C:\events\psdaych2024\demos\02-finding-placeholders\files' | ft
 
 #should feed the file statistics ref variable with the used placeholders in all files underneath the specified folder
+cls
 [ref]$fs = $Null
 Get-PSPlaceholder -Path 'C:\events\psdaych2024\demos\02-finding-placeholders\files' -FileStatistics $fs
 $fs.Value | ft name,PlaceholderInName,PlaceholderInContent
--- demos/03-replacing-placeholders/03-replacing-placeholders.ps1
+++ demos/03-replacing-placeholders/03-replacing-placeholders.ps1
@@ -1,14 +1,17 @@
 #should complain about missing FirstName placeholder value.
+cls
 Update-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' -Values @{
   LastName  ='Dow'
 }
 
 #should skip missing FirstName placeholder value.
+cls
 Update-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' -Values @{
   LastName  ='Dow'
 } -AllowEmptyPlaceholders
 
 #should warn for unused placeholder values
+cls
 Update-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' -Values @{
   FirstName ='John'
   LastName  ='Dow'
@@ -17,6 +20,7 @@ Update-PSPlaceholder -String 'My name is {{FirstName}} {{LastName}}' -Values @{
 }
 
 #It behaves the same way when you work with Files and Folders
+cls
 Update-PSPlaceholder -Path 'C:\events\psdaych2024\demos\03-replacing-placeholders\files' -Values @{
   LastName  ='Dow'
   someStrangeValue = 'someStrangeValue'
